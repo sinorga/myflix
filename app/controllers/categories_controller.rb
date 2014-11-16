85aa044f9270b3d70334634224b952bc4bd895e7
orga <orga.shih@gmail.com>
2014-11-16 16:28:34 +0800

    add category show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,8 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.includes(:videos).all
   end
+
+  def show
+    @category = Category.includes(:videos).find(params[:id])
+  end
 end
