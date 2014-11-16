ec560c9cb04c6e6b5686fc7ca1e6a983f2a34796
orga <orga.shih@gmail.com>
2014-11-16 21:35:13 +0800

    add a simple video test

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Video do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "saves itself" do
+    video = Video.new(title: "ID4", description: "hehehe")
+    video.save
+    expect(Video.first).to eq(video)
+  end
 end
