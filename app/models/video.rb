352911de4e98d2e503cabe11b898895f567086f8
orga <orga.shih@gmail.com>
2014-11-18 01:19:35 +0800

    Implement 'search_by_title' on the video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,9 @@
 class Video < ActiveRecord::Base
   belongs_to :category
   validates_presence_of :title, :description
+
+  def self.search_by_title(title)
+    return [] if title.blank?
+    where("title LIKE ?", "%#{title}%").order(created_at: :desc)
+  end
 end
