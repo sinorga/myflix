0d8711be9a284cc1431adc2a16caa4fcccfe2d9e
orga <orga.shih@gmail.com>
2014-11-29 15:33:45 +0800

    correct rating range from 0..5 to 1..5

--- spec/fabricators/review_fabricator.rb
+++ spec/fabricators/review_fabricator.rb
@@ -1,4 +1,4 @@
 Fabricator(:review) do
-  rating { (0..5).to_a.sample }
+  rating { (1..5).to_a.sample }
   content { Faker::Lorem.paragraph }
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,6 +5,6 @@ describe Review do
   it { should belong_to(:video) }
   it { should validate_presence_of(:rating) }
   it { should validate_presence_of(:content) }
-  it { should validate_inclusion_of(:rating).in_range(0..5) }
+  it { should validate_inclusion_of(:rating).in_range(1..5) }
 
 end
