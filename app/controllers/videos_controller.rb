db9cddcbb61307e075e6c62bf46c8ff2eaf0c414
orga <orga.shih@gmail.com>
2014-11-29 15:29:46 +0800

    refactoring: assigns @reviews variable in GET show video controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,8 +1,9 @@
 class VideosController < ApplicationController
   before_action :require_user
-  
+
   def show
     @video = Video.find(params[:id])
+    @reviews = @video.reviews
   end
 
   def search
--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -9,6 +9,15 @@ describe VideosController do
       expect(assigns(:video)).to eq(video)
     end
 
+    it "sets @reviews for authenticated users" do
+      request.session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
+      reviews = Fabricate.times(5, :review, video: video)
+
+      get :show, id: video.id
+      expect(assigns(:reviews)).to match_array(reviews)
+    end
+
     it "redirects the unauthenticated user to sign in page" do
       video = Fabricate(:video)
       get :show, id: video.id
