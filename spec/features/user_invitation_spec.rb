5f75ae2856f69d23e52cd8b4b2bbf624c9d2c49c
orga <orga.shih@gmail.com>
2015-02-06 01:32:55 +0800

    refactor: exract method in invitation feature spec

--- spec/features/user_invitation_spec.rb
+++ spec/features/user_invitation_spec.rb
@@ -7,31 +7,41 @@ feature "User invites friend" do
     bob = Fabricate.build(:user, email: invite_bob.email)
     sign_in(alice)
 
-    click_on "Invite a friend"
-    fill_in("Friend's Name", with: invite_bob.name)
-    fill_in("Friend's Email Address", with: invite_bob.email)
-    fill_in("Invitation Message", with: invite_bob.message)
-    click_on "Send Invitation"
-    sign_out
-
-    open_email(invite_bob.email)
-    expect(current_email).to have_content(invite_bob.name)
-    expect(current_email).to have_content(invite_bob.message)
-    current_email.click_link "Register"
-
-    expect_have_hidden_field('invite_token', InviteUser.last.token)
-    expect_have_prefilled_email(invite_bob)
-    fill_in("Password", with: bob.password)
-    fill_in("Full Name", with: bob.full_name)
-    click_on "Sign Up"
-    expect(current_path).to eq("/sign_in")
+    invite_a_friend(invite_bob)
+    friend_registration_from_email(invite_bob)
+    friend_sign_up(bob)
 
     expect_have_followed_relationship(alice, bob)
     expect_have_followed_relationship(bob, alice)
   end
 
-  def expect_have_prefilled_email(invite_user)
-    expect(find_field('Email Address').value).to eq(invite_user.email)
+  def invite_a_friend(invited_friend)
+    click_on "Invite a friend"
+    fill_in("Friend's Name", with: invited_friend.name)
+    fill_in("Friend's Email Address", with: invited_friend.email)
+    fill_in("Invitation Message", with: invited_friend.message)
+    click_on "Send Invitation"
+    sign_out
+  end
+
+  def friend_registration_from_email(invited_friend)
+    open_email(invited_friend.email)
+    expect(current_email).to have_content(invited_friend.name)
+    expect(current_email).to have_content(invited_friend.message)
+    current_email.click_link "Register"
+  end
+
+  def friend_sign_up(friend)
+    expect_have_hidden_field('invite_token', InviteUser.last.token)
+    expect_have_prefilled_email(friend)
+    fill_in("Password", with: friend.password)
+    fill_in("Full Name", with: friend.full_name)
+    click_on "Sign Up"
+    expect(current_path).to eq("/sign_in")
+  end
+
+  def expect_have_prefilled_email(friend)
+    expect(find_field('Email Address').value).to eq(friend.email)
   end
 
   def expect_have_hidden_field(id, value)
