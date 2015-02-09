de3fee685cf36feb9414074ab00a80857a672aef
orga <orga.shih@gmail.com>
2015-02-10 00:00:42 +0800

    Assignment: Use Mailgun to send out emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,12 +20,12 @@ Myflix::Application.configure do
 
   config.action_mailer.default_url_options = { :host => ENV['APP_HOST'] }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               ENV['EMAIL_DOMAIN'],
-    user_name:            ENV['EMAIL_USERNAME'],
-    password:             ENV["EMAIL_PASSWORD"],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'orgaflix.heroku.com',
+    :authentication => :plain,
+  }
 end
