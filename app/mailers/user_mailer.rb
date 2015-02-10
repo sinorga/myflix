8f0437c9fec0349c9f471bffd1b8bbf4cf043c3a
orga <orga.shih@gmail.com>
2015-02-11 02:11:51 +0800

    add staging config and check env in user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,16 +2,21 @@ class UserMailer < ActionMailer::Base
   default from: "info@myfilx.com"
   def welcome(user)
     @user = user
-    mail to: user.email, subject: "Welcome! mylifx"
+    mail to: send_to(user), subject: "Welcome! mylifx"
   end
 
   def password_reset_notify(user)
     @user = user
-    mail to: user.email, subject: "mylifx reset password"
+    mail to: send_to(user), subject: "mylifx reset password"
   end
 
   def send_invitation(invitation)
     @invitation = invitation
-    mail to: invitation.email, subject: "mylifx, #{invitation.inviter.full_name} invite you!"
+    mail to: send_to(invitation), subject: "mylifx, #{invitation.inviter.full_name} invite you!"
+  end
+
+  protected
+  def send_to(target)
+    Rails.env.staging? ? ENV['STAGING_EMAIL'] : target.email
   end
 end
