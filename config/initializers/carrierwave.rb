a0e47aec5c7aacfe7b2c53445f6e5b522bb6a9ee
orga <orga.shih@gmail.com>
2015-02-16 21:29:07 +0800

    Assignment: Admin adds a video (store file in AWS S3)

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,6 +5,14 @@ if Rails.env.test? or Rails.env.cucumber?
   end
 else
   CarrierWave.configure do |config|
-    config.storage = :file
+    config.storage = :fog
+
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['S3_KEY'],
+      :aws_secret_access_key  => ENV['S3_SECRET'],
+      :region                 => 'ap-northeast-1',
+    }
+    config.fog_directory  = ENV['S3_BUCKET']
   end
 end
