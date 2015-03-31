f38e7b72f427497eb3f5549551673ad19742cce7
orga <orga.shih@gmail.com>
2015-03-31 22:53:06 +0800

    Assignment: Build API wrapper for Stripe(sol)

--- spec/models/stripe_wrapper_spec.rb
+++ spec/models/stripe_wrapper_spec.rb
@@ -1,36 +1,42 @@
 require 'spec_helper'
 
-describe StripeWrapper::Charge do
-  let(:token) do
-    Stripe::Token.create(
-      :card => {
-        :number => card_number,
-        :exp_month => 3,
-        :exp_year => 2016,
-        :cvc => "314"
-      },
-    ).id
-  end
-  context "with valid card" do
-    let(:card_number) { '4242424242424242' }
-    it "charges the card successfully", :vcr do
-      expect{
-        charge = StripeWrapper::Charge.create(
-          :amount => 999, # amount in cents, again
-          :source => token,
-        )
-      }.to_not raise_error
-    end
-  end
-  context "with invalid card" do
-    let(:card_number) { '4000000000000002' }
-    it "raise an exception with error message", :vcr do
-      expect{
-        charge = StripeWrapper::Charge.create(
-          :amount => 999, # amount in cents, again
-          :source => token,
-        )
-      }.to raise_error(StripeWrapper::CardError, "Your card was declined.")
+describe StripeWrapper do
+  describe StripeWrapper::Charge do
+    describe ".create" do
+      let(:token) do
+        Stripe::Token.create(
+          :card => {
+            :number => card_number,
+            :exp_month => 3,
+            :exp_year => 2016,
+            :cvc => "314"
+          },
+        ).id
+      end
+
+      context "with valid card" do
+        let(:card_number) { '4242424242424242' }
+        it "charges the card successfully", :vcr do
+          charge = StripeWrapper::Charge.create(
+              :amount => 999, # amount in cents, again
+              :source => token,
+          )
+          expect(charge.amount).to eq(999)
+          expect(charge.currency).to eq('usd')
+        end
+      end
+
+      context "with invalid card" do
+        let(:card_number) { '4000000000000002' }
+        it "raise an exception with error message", :vcr do
+          expect{
+            charge = StripeWrapper::Charge.create(
+              :amount => 999, # amount in cents, again
+              :source => token,
+            )
+          }.to raise_error(StripeWrapper::CardError, "Your card was declined.")
+        end
+      end
     end
   end
 end
