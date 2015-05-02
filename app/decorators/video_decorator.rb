2f8950a22424188d369439c9360a108a81dfb72f
orga <orga.shih@gmail.com>
2015-05-02 08:25:02 +0800

    rename method of video_decorator

--- app/decorators/video_decorator.rb
+++ app/decorators/video_decorator.rb
@@ -2,11 +2,11 @@ class VideoDecorator < Draper::Decorator
   delegate_all
 
   def show_rating
-    average_rating ? "#{show_average_rating}/5.0" : "N/A"
+    average_rating ? "#{rounded_average_rating}/5.0" : "N/A"
   end
 
   private
-  def show_average_rating
+  def rounded_average_rating
     average_rating.round(1)
   end
 end
